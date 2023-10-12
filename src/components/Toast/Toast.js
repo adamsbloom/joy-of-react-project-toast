1285d627d659dc26d5d780e929d6e8ce8cb38582
abloom <abloom@schonfeld.com>
2023-10-12 11:45:01 -0400

    exercise 2 - toast preview

--- src/components/Toast/Toast.js
+++ src/components/Toast/Toast.js
@@ -1,15 +1,15 @@
-import React from 'react';
+import React from "react";
 import {
   AlertOctagon,
   AlertTriangle,
   CheckCircle,
   Info,
   X,
-} from 'react-feather';
+} from "react-feather";
 
-import VisuallyHidden from '../VisuallyHidden';
+import VisuallyHidden from "../VisuallyHidden";
 
-import styles from './Toast.module.css';
+import styles from "./Toast.module.css";
 
 const ICONS_BY_VARIANT = {
   notice: Info,
@@ -18,16 +18,15 @@ const ICONS_BY_VARIANT = {
   error: AlertOctagon,
 };
 
-function Toast() {
+function Toast({ variant, onClose, children }) {
+  const variantStyles = variant ? styles[variant] : "";
+  const Icon = variant ? ICONS_BY_VARIANT[variant] : null;
+
   return (
-    <div className={`${styles.toast} ${styles.notice}`}>
-      <div className={styles.iconContainer}>
-        <Info size={24} />
-      </div>
-      <p className={styles.content}>
-        16 photos have been uploaded
-      </p>
-      <button className={styles.closeButton}>
+    <div className={`${styles.toast} ${variantStyles}`}>
+      <div className={styles.iconContainer}>{Icon && <Icon size={24} />}</div>
+      <p className={styles.content}>{children}</p>
+      <button className={styles.closeButton} onClick={onClose}>
         <X size={24} />
         <VisuallyHidden>Dismiss message</VisuallyHidden>
       </button>
